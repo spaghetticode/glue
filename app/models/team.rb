2f8913a6b2d7b86b77fdc8badb0bce3c072bad32
andrea longhi <andrea@spaghetticode.it>
2013-09-12 15:28:07 +0200

    green team specs

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,13 @@
 class Team < ActiveRecord::Base
+  DUMMY_NAME = 'foggia'
+
+  validates :name, :presence => true
+
+  before_validation :set_dummy_name
+
+  private
+
+  def set_dummy_name
+    self.name = DUMMY_NAME unless name.present?
+  end
 end
