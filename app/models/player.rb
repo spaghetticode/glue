0d7d30cd66bb0769451b9c046a82bc87c0477fc2
andrea longhi <andrea@spaghetticode.it>
2013-09-12 14:22:55 +0200

    add player model code

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,14 @@
 class Player < ActiveRecord::Base
+  DUMMY_NAME = 'john smith'
+
+  validates :name, :rfid, presence: true
+  validates :rfid, length: {is: 12}
+
+  before_validation :set_dummy_name
+
+  private
+
+  def set_dummy_name
+    self.name = DUMMY_NAME
+  end
 end
