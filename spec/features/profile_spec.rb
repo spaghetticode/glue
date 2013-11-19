a0c80c0042961f096df2ebae37530a5b3ba0a1c3
andrea longhi <andrea@spaghetticode.it>
2013-11-19 17:39:41 +0100

    update profile page

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -4,12 +4,12 @@ describe 'profile' do
   let(:password) { 'secret123' }
   let(:player) { create_player :password => password, :password_confirmation => password }
 
-  before do
-    login player, password
-  end
+  before { login player, password }
 
   it 'shows the user profile' do
     visit profile_path
     page.should have_content 'Profile'
+    page.should have_content player.email
+    page.should have_content 'edit profile'
   end
 end
