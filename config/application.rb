fa38c974a55f9fba7a24c55e3671cee798cd8d41
andrea longhi <andrea@spaghetticode.it>
2013-11-19 14:42:04 +0100

    disable automatic coffee and css generation

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Glue
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
   end
 end
