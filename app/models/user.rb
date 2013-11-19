fc8364394f2ad242f9be26ea833a47addef0ccb6
andrea longhi <andrea@spaghetticode.it>
2013-11-19 13:58:01 +0100

    add important comment about authentication via user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # FIXME: not sure this is what I want. We need to register players, and players should be allowed to log in.
+  # Player model already exists, so that model should be enhanched with devise. User model is basically useless.
 end
