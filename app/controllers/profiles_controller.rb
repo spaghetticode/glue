8ca5b5d791e54e923323ca97593e38e0cadf37a0
andrea longhi <andrea@spaghetticode.it>
2013-11-21 16:16:13 +0100

    working profile editing

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,23 @@
 class ProfilesController < AuthenticatedController
   def show
+    # nothing here
   end
 
   def edit
+    # neither here
   end
 
   def update
+    if current_player.update_attributes(profile_params)
+      redirect_to profile_path, notice: 'Your profile was successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:player).permit(Player.column_names)
   end
 end
--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe 'profile' do
   let(:password) { 'secret123' }
-  let(:player) { create_player :password => password, :password_confirmation => password }
+  let(:new_rfid) { 'somethingfun' }
+  let(:player)   { create_player :password => password, :password_confirmation => password }
 
   before { login player, password }
 
@@ -11,5 +12,13 @@ describe 'profile' do
     page.should have_content 'Profile'
     page.should have_content player.email
     page.should have_content 'edit profile'
+  end
+
+  it 'updates the user profile' do
+    visit edit_profile_path
+    fill_in 'Rfid', with: new_rfid
+    click_button :update
+    page.should have_content 'Your profile was successfully updated'
+    page.should have_content new_rfid
   end
 end
