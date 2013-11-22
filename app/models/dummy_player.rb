e84e9cbcab71f1717b0cb5548bb13977f7b47b75
andrea longhi <andrea@spaghetticode.it>
2013-11-22 11:50:38 +0100

    dont require email for DummyPlayer

--- app/models/dummy_player.rb
+++ app/models/dummy_player.rb
@@ -1,2 +1,5 @@
 class DummyPlayer < Player
+  validates :rfid, uniqueness: true
+  # TODO find better way to skip email validation for this subclass
+  validates_presence_of :email, :if => Proc.new { false } # no email validation for DummyPlayer
 end
