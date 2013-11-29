3478f4a5ee214273c53c394e6641ef5d14d3d8ab
andrea longhi <andrea@spaghetticode.it>
2013-11-29 10:56:40 +0100

    add explicit response

--- app/controllers/ws/matches_controller.rb
+++ app/controllers/ws/matches_controller.rb
@@ -6,16 +6,19 @@ module Ws
       @factory = MatchFactory.new(params)
       @factory.save
       broadcast_message :refresh_match, msg_body: match.to_json
+      send_message :started, match.to_json
     end
 
     def close
       match.close
       broadcast_message :refresh_match, msg_body: match.to_json
+      send_message :closed, match.to_json
     end
 
     def update
       match.update_score(params)
       broadcast_message :refresh_match, msg_body: match.to_json
+      send_message :updated, match.to_json
     end
 
     private
