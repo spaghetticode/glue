231daa245b25fcb46b209c8a859563aaba6d3c6d
andrea longhi <andrea@spaghetticode.it>
2013-11-29 17:07:33 +0100

    add twitting

--- app/controllers/ws/matches_controller.rb
+++ app/controllers/ws/matches_controller.rb
@@ -7,12 +7,15 @@ module Ws
       @factory.save
       broadcast_message :refresh_match, msg_body: match.to_json
       send_message :started, match.to_json
+      Social.new.tweet "#{timestamp} A new match has started! #{twitter_names}"
+
     end
 
     def close_match
       match.close
       broadcast_message :refresh_match, msg_body: match.to_json
       send_message :closed, match.to_json
+      Social.new.tweet "#{timestamp} Match result: #{match.team_a.score} - #{match.team_b.score} #{twitter_names}"
     end
 
     def update_match
@@ -29,6 +32,14 @@ module Ws
 
     def params
       message
+    end
+
+    def timestamp
+      Time.now.strftime '%H:%M'
+    end
+
+    def twitter_names
+      "#{match.player_1.twitter_name}, #{match.player_2.twitter_name} VS #{match.player_3.twitter_name}, #{match.player_4.twitter_name}"
     end
   end
 end
