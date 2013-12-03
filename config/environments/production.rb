e534b45ee1042e122e8d5f4dbc39582cb524a9e8
andrea longhi <andrea@spaghetticode.it>
2013-12-03 11:41:56 +0100

    let rails serve static assets in production
    use asset digested names for background image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ Glue::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
+  # config.serve_static_assets = false
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
