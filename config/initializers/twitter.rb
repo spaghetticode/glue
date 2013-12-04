9c4224c526bfeb975f86c519669b088bb1e54538
andrea longhi <andrea@spaghetticode.it>
2013-12-04 17:03:06 +0100

    use twitter.yml.sample when twitter.yml doesnt exist

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,10 @@
 require 'twitter'
 
-TWITTER = YAML.load_file Rails.root.join('config/twitter.yml')
+config = Rails.root.join('config/twitter.yml')
+dummy  = Rails.root.join('config/twitter.yml.sample')
+config_file = config.exist? ? config : dummy
+
+TWITTER = YAML.load_file config_file
 
 Twitter.configure do |config|
   config.consumer_key       = TWITTER['key']
