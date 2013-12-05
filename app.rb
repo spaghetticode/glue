6b14d7fb9b2bc5da7558c7a684347c2686b13bb1
andrea longhi <andrea@spaghetticode.it>
2013-12-05 10:08:42 +0100

    extract index.erb view

--- app.rb
+++ app.rb
@@ -13,6 +13,13 @@ matches = []
 
 
 get '/' do
+  @match = Match.new(
+    :player_1 => 'maradona',
+    :player_2 => 'falcao',
+    :player_3 => 'pelé',
+    :player_4 => 'bergomi',
+    :start_at => Time.now
+  )
   erb :index
 end
 
@@ -47,38 +54,3 @@ get '/websocket' do
     end
   end
 end
-
-__END__
-@@ index
-<!DOCTYPE html><html lang="en">
-<head>
-  <meta charset="utf-8" />
-  <title>Biliarduino</title>
-  <link href="/css/match.css" media="all" rel="stylesheet" />
-  <script src="/js/jquery.js"></script>
-  <script src="/js/match.js"></script>
-</head>
-<body>
-  <div data-wsuri="localhost:3000/websocket" id="match">
-    <div data-clock="true" id="clock"></div>
-    <div class="flexcontainer">
-      <section id="set-1">
-        <div class="hi-icon-wrap hi-icon-effect-1 hi-icon-effect-1a">
-          <a class="hi-icon" href="#set-1" id="score_a"></a>
-          <ul>
-            <li class="player" id="player_1"></li><li class="player" id="player_2"></li>
-          </ul>
-        </div>
-      </section>
-      <section id="set-2"><div class="hi-icon-wrap hi-icon-effect-2 hi-icon-effect-2a">
-        <a class="hi-icon" href="#set-2" id="score_b"></a>
-        <ul>
-          <li class="player" id="player_3"></li><li class="player" id="player_4"></li>
-        </ul>
-      </div>
-    </section>
-  </div>
-</div>
-<footer><p>&copy; <a href="https://www.mikamai.com">MIKAMAI 2013</a></p></footer>
-</body>
-</html>
