e150ddd085a112336b80ddcb542d0bbc588d349b
andrea longhi <andrea@spaghetticode.it>
2013-12-05 11:42:19 +0100

    migrate match class to activerecord

--- match.rb
+++ match.rb
@@ -1,34 +1,23 @@
-class Match
-  attr_writer   :team_a_score, :team_b_score, :closed
-  attr_accessor :player_1, :player_2, :player_3, :player_4, :start_at, :end_at
+class Match < ActiveRecord::Base
+  # attr_writer   :team_a_score, :team_b_score, :closed
+  # attr_accessor :player_1, :player_2, :player_3, :player_4, :start_at, :end_at
+  before_create :set_start_at
 
-  def initialize(players)
-    update(players)
-  end
-
-  def update(attributes)
-    attributes.each do |attribute, value|
-      send "#{attribute}=", value
-    end
-  end
-
-  def close
-    self.closed = true
+  def close(attributes)
+     update_attributes attributes.merge(end_at: Time.now)
   end
 
   def closed?
-    !!@closed
-  end
-
-  def team_a_score
-    @team_a_score ||= 0
-  end
-
-  def team_b_score
-    @team_b_score ||= 0
+    !!end_at
   end
 
   def to_json
     %({"player_1": "#{player_1}","player_2": "#{player_2}","player_3": "#{player_3}","player_4": "#{player_4}", "team_a_score": "#{team_a_score}", "team_b_score": "#{team_b_score}", "closed": "#{closed?}"})
+  end
+
+  private
+
+  def set_start_at
+    self.start_at ||= Time.now
   end
 end
