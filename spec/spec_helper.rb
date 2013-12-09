bb9914c4077d7b4920307254d788f47d1f5e87ca
andrea longhi <andrea@spaghetticode.it>
2013-12-09 15:49:29 +0100

    use database_cleaner in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,26 @@ ENV['RACK_ENV'] = 'test'
 
 require 'rack'
 require 'rack/test'
+require 'database_cleaner'
 require_relative '../app'
+
+DatabaseCleaner.strategy = :truncation
 
 module RSpecMixin
   include Rack::Test::Methods
   def app() Sinatra::Application end
 end
 
-RSpec.configure { |c| c.include RSpecMixin }
+RSpec.configure do |config|
+  config.before :suite do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
+  config.before:each do
+    DatabaseCleaner.start
+  end
+  config.after :each do
+    DatabaseCleaner.clean
+  end
+  config.include RSpecMixin
+end
