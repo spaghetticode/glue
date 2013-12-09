49553a31704f2488542c37b212b1dcb9e943728e
andrea longhi <andrea@spaghetticode.it>
2013-12-09 14:45:49 +0100

    dont log Activerecord in test

--- environment.rb
+++ environment.rb
@@ -3,10 +3,11 @@ require 'sinatra-websocket'
 Dir['models/*'].each { |file| require_relative file }
 
 def db_connect
+  env = Sinatra::Application.environment
   ActiveRecord::Base.establish_connection(
     adapter:  'sqlite3',
-    database: "db/glue_#{Sinatra::Application.environment}.sqlite3"
+    database: "db/glue_#{env}.sqlite3"
   )
-  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  ActiveRecord::Base.logger = Logger.new(STDOUT) unless env == :test
   ActiveRecord::Migration.verbose = true
 end
