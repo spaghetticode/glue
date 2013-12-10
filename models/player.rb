48f4435853fdf133ab3935d9ba266d10eebfe1f4
andrea longhi <andrea@spaghetticode.it>
2013-12-10 16:00:12 +0100

    add Player::from_data

--- models/player.rb
+++ models/player.rb
@@ -1,13 +1,24 @@
 class Player < ActiveRecord::Base
   before_save :set_twitter_name
 
-  has_many :matches
+  def self.from_data(data)
+    const_get(data[:type]).find_or_create_from_data data
+  end
+
+  def self.add_at(name)
+    name[0] == '@' ? name : name.prepend('@')
+  end
+
+
+  def matches
+    Match.where 'player_1_id = :id or player_2_id = :id or player_3_id = :id or player_4_id = :id', id: id
+  end
 
   private
 
   def set_twitter_name
     if twitter_name
-      twitter_name.prepend('@') unless twitter_name.first == '@'
+      self.twitter_name = self.class.add_at(twitter_name)
     end
   end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -10,4 +10,13 @@ describe Player do
     player = Player.create(twitter_name: '@babbeo')
     player.twitter_name.should == '@babbeo'
   end
+
+  describe '::from_data' do
+    let(:data) { {twitter_name: 'asd', type: 'DummyPlayer'} }
+
+    it 'delegates to the subclass' do
+      DummyPlayer.should_receive :find_or_create_from_data
+      Player.from_data(data)
+    end
+  end
 end
