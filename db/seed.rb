97ec6de93964f3ad225f6abd99186752fcf93b4e
andrea longhi <andrea@spaghetticode.it>
2013-12-10 16:47:55 +0100

    update seed file

--- db/seed.rb
+++ db/seed.rb
@@ -1,12 +1,20 @@
-  puts 'create sample match'
-  Match.create(
-    player_1: 'maradona',
-    player_2: 'falcao',
-    player_3: 'pelé',
-    player_4: 'bergomi',
-    start_at: Time.now - 600,
-    end_at:   Time.now
-  )
+puts 'create sample match'
+%w[maradona falcao bergomi pelè].each do |name|
+  DummyPlayer.create twitter_name: name
+end
 
-  puts 'create table settings with default values'
-  TableSettings.create
+players = Player.all
+
+Match.create(
+  player_1: players[0],
+  player_2: players[1],
+  player_3: players[2],
+  player_4: players[3],
+  start_at: Time.now - 600,
+  end_at:   Time.now
+)
+
+
+
+puts 'create table settings with default values'
+TableSettings.create
