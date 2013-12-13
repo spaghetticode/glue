d3e00d06ccfc23f4a8e25724c1b1a15b3dc4766d
andrea longhi <andrea@spaghetticode.it>
2013-12-13 10:34:37 +0100

    add more seed data

--- db/seed.rb
+++ db/seed.rb
@@ -6,17 +6,19 @@ end
 players = Player.all
 
 now = Time.now
-match = Match.create(
-  player_1: players[0],
-  player_2: players[1],
-  player_3: players[2],
-  player_4: players[3],
-  team_a_score: 6,
-  team_b_score: 3
-)
-match.close
-match.update_attributes(start_at: now - 600, end_at: now)
 
+5.times do |n|
+  match = Match.create(
+    player_1: players[0],
+    player_2: players[1],
+    player_3: players[2],
+    player_4: players[3],
+    team_a_score: n+5,
+    team_b_score: n+3
+  )
+  match.close
+  match.update_attributes(start_at: now - 600 - n*300, end_at: now - n*300)
+end
 
 puts 'create table settings with default values'
 TableSettings.create
