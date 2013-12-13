5ace670d1b4f919540e40dc005eab1ee931c6094
andrea longhi <andrea@spaghetticode.it>
2013-12-13 14:28:10 +0100

    settings#advantages now defaults to 2

--- db/migrations/002_create_table_settings.rb
+++ db/migrations/002_create_table_settings.rb
@@ -2,7 +2,7 @@ class CreateTableSettings < ActiveRecord::Migration
   def up
     create_table :table_settings do |t|
       t.integer  :goals,       default: 8
-      t.integer  :advantages,  default: 1
+      t.integer  :advantages,  default: 2
       t.integer  :max_minutes, default: nil
       t.timestamps
     end
