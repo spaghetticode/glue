40319f35d7f760c2f5691a4503694062b5999175
andrea longhi <andrea@spaghetticode.it>
2013-12-16 11:33:58 +0100

    fix load error when twitte.ryml is missing

--- models/social.rb
+++ models/social.rb
@@ -2,24 +2,30 @@ require 'yaml'
 require 'twitter'
 
 file = File.expand_path('../../config/twitter.yml', __FILE__)
-yml  = YAML.load_file file
+conf = File.file?(file) ? YAML.load_file(file) : {}
 
 Twitter.configure do |config|
-  config.consumer_key       = yml['key']
-  config.consumer_secret    = yml['secret']
-  config.oauth_token        = yml['oauth_token']
-  config.oauth_token_secret = yml['oauth_secret']
+  config.consumer_key       = conf['key']
+  config.consumer_secret    = conf['secret']
+  config.oauth_token        = conf['oauth_token']
+  config.oauth_token_secret = conf['oauth_secret']
 end
+
 
 module Social
   extend self
 
   def tweet(message)
+    return unless twitter_enabled?
     begin
       Twitter.update message
     rescue => e
       puts "TWITTER ERROR: #{e.message}"
       puts e.backtrace
     end
+  end
+
+  def twitter_enabled?
+    Twitter.consumer_key.present?
   end
 end
